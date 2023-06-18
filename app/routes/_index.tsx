60cbc66df847ba4a97b5d3f0585ccdcf5b4a8fec
hyu_wang <hyu_wang@trip.com>
2023-06-18 16:26:15 +0800

    feat: remix fetchdata

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,18 +2,39 @@ import type { LoaderFunction } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 
-export const loader: LoaderFunction = () => {
+function getComments(): Promise<string[]> {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(['This is Great.', 'Worthy of recommendation!']);
+    }, 3000)
+  );
+}
+
+export const loader: LoaderFunction = async () => {
+  const comments = await getComments();
+
   return json({
-    name: '19Qingfeng',
+    comments,
   });
 };
 
 export default function Index() {
-  const { name } = useLoaderData();
+  const { comments } = useLoaderData<{ comments: string[] }>();
 
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h3>Hello {name}!</h3>
+      <div>
+        <div>商品</div>
+        <p>价格</p>
+        <div>
+          <div>
+            <p>评论</p>
+            {comments.map((comment) => {
+              return <p key={comment}>{comment}</p>;
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
